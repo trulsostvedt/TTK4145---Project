a0f1c882a54a5292d5119267a064cd6d7a3934b4
jacobgo1 <jacob.golsson@gmail.com>
2025-02-18 14:09:07 +0100

    Remove driver-go subproject and add Elevator struct to main.go

--- main.go
+++ main.go
@@ -5,6 +5,19 @@ import (
 	"TTK4145---project/Broadcast"
 )
 
+
+
+type Elevator struct {
+	ID int
+	State int
+	Direction int
+	Floor int
+	OrderMatrix [4][3]int
+}
+
+
+
+
 func main() {
 	go broadcast.Startbroadcast()
 	go broadcast.ListenForBroadcast()
